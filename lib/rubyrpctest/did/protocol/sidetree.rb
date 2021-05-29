7be7484c1816b12cdadc5380d597696f2f1d073d
wokashi <tkm05oshikawa12@gmail.com>
2021-05-29 22:10:01 +0900

    fix

--- lib/rubyrpctest/did/protocol/sidetree.rb
+++ lib/rubyrpctest/did/protocol/sidetree.rb
@@ -2,10 +2,11 @@ module Rubyrpctest
   class DID
     module Protocol
       autoload :Operations, 'rubyrpctest/did/protocols/sidetree/operations'
+
       class Sidetree
 
         def createDid
-          Operations.createOperation
+          Operations.create_operation
         end
 
         def resolve
--- lib/rubyrpctest/did/protocol/sidetree/operations/enum/operation_type.rb
+++ lib/rubyrpctest/did/protocol/sidetree/operations/enum/operation_type.rb
@@ -1,16 +1,16 @@
-  module Rubyrpctest
-    class DID
-      module Protocol
-        class Sidetree
-          module Operations
-            module OperationType
-              Create = 'create'
-              Update = 'update'
-              Deactivate = 'deactivate'
-              Recover = 'recover'
-            end
+module Rubyrpctest
+  class DID
+    module Protocol
+      class Sidetree
+        module Operations
+          module OperationType
+            Create = 'create'
+            Update = 'update'
+            Deactivate = 'deactivate'
+            Recover = 'recover'
           end
         end
       end
     end
   end
+end
