00b49a635b676bc5dc57d46d93655d7c4c442712
wokashi <tkm05oshikawa12@gmail.com>
2021-05-29 22:22:45 +0900

    fix

--- lib/rubyrpctest/did/utils.rb
+++ lib/rubyrpctest/did/utils.rb
@@ -1,10 +1,10 @@
 module Rubyrpctest
   class DID
     module Utils
-      autoload :JsonCanonicalizer, 'rubyrpctest/utils/json_canonicalizer'
-      autoload :Hash, 'rubyrpctest/utils/hash'
-      autoload :Encoder, 'rubyrpctest/utils/encoder'
-      autoload :Key, 'rubyrpctest/utils/key'
+      autoload :JsonCanonicalizer, 'rubyrpctest/did/utils/json_canonicalizer'
+      autoload :Hash, 'rubyrpctest/did/utils/hash'
+      autoload :Encoder, 'rubyrpctest/did/utils/encoder'
+      autoload :Key, 'rubyrpctest/did/utils/key'
     end
   end
 end
