7e2b0809067dc4509ec8e5cea9b3ae442c1d6e2a
wokashi <tkm05oshikawa12@gmail.com>
2021-05-29 22:15:40 +0900

    fix

--- lib/rubyrpctest/did/utils.rb
+++ lib/rubyrpctest/did/utils.rb
@@ -4,6 +4,7 @@ module Rubyrpctest
       autoload :JsonCanonicalizer, 'rubyrpctest/utils/json_canonicalizer'
       autoload :Hash, 'rubyrpctest/utils/hash'
       autoload :Encoder, 'rubyrpctest/utils/encoder'
+      autoload :Key, 'rubyrpctest/utils/key'
     end
   end
 end
